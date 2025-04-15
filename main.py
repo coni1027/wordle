870d945cb4b18a8c6c316742577ee0eb018eb810
coni1027 <nikolabitoria@gmail.com>
2025-04-16 01:43:17 +0800

    Created keyboard layout
    todo:
    - make list of dictionary for each letter while making grid
    - make keyboard with letter

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ screen_width = 900
 screen_height = 600
 cell_size = 50
 cell_margin = 5
+key_width = 30
+key_height = 40
+key_width_margin = 5
+key_height_margin = 5
 
 def drawHUD():
     fps = clock.get_fps()
@@ -28,10 +32,13 @@ while running:
 
     grid_x_pos = (screen_width // 2) - (cell_size*2.5) - (cell_margin*2.5)
     grid_y_pos = 30
+    key_x_pos = (screen_width // 2) - (key_width*5) - (key_width_margin*5)
+    key_y_pos = 400
 
     screen.fill("white")
     drawHUD()
 
+    # draw grid
     for row in range(6):
         for column in range(5):
             pygame.draw.rect(screen,"darkgray",pygame.Rect(grid_x_pos,grid_y_pos,cell_size,cell_size),2)
@@ -39,6 +46,24 @@ while running:
         grid_x_pos = (screen_width // 2) - (cell_size*2.5) - (cell_margin*2.5)
         grid_y_pos += cell_size + cell_margin
     
-
+    # draw keyboard layout
+    for column in range(10):
+        pygame.draw.rect(screen,"lightgray",pygame.Rect(key_x_pos,key_y_pos,key_width,key_height))
+        key_x_pos += key_width + key_width_margin
+    key_x_pos = (screen_width // 2) - (key_width*5) - (key_width_margin*5) + key_width // 2 + key_width_margin // 2
+    key_y_pos += key_height + key_height_margin
+    for column in range(9):
+        pygame.draw.rect(screen,"lightgray",pygame.Rect(key_x_pos,key_y_pos,key_width,key_height))
+        key_x_pos += key_width + key_width_margin
+    key_x_pos = (screen_width // 2) - (key_width*5) - (key_width_margin*5)
+    key_y_pos += key_height + key_height_margin
+    pygame.draw.rect(screen,"lightgray",pygame.Rect(key_x_pos,key_y_pos,key_width+18,key_height))
+    key_x_pos += key_width + key_width_margin+18
+    for column in range(7):
+        pygame.draw.rect(screen,"lightgray",pygame.Rect(key_x_pos,key_y_pos,key_width,key_height))
+        key_x_pos += key_width + key_width_margin
+    pygame.draw.rect(screen,"lightgray",pygame.Rect(key_x_pos,key_y_pos,key_width+18,key_height))        
+    
+    
     pygame.display.update()
     clock.tick(60)
